c5a6606abfb4b18973b93bff1ff7b918357d2beb
Abegunde Precious Olanrewaju <preciousolanrewaju1998@gmail.com>
2022-07-24 12:39:42 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge:[
+    "*.html"
+  ],
+  content: [],
+  mode: 'jit',
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Fira sans','sans-serif']
+    }
+  },
+  plugins: [],
+}
